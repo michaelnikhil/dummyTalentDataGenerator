72c36f321783249c9d604c50d2f207d443e07dfd
michaelnikhil <michael.n.descamps@gmail.com>
2020-04-19 21:16:30 +0200

    Initial commit

--- generate_data.py
+++ generate_data.py
@@ -9,16 +9,19 @@ import pandas as pd
 import random
 import mysql.connector
 
+#separate file with DB credentials
+from my_db_credentials import mydb
+
 """ DATABASE SETTINGS """
 INSERT = True
 
-#db connection information
-mydb = mysql.connector.connect(
-  host="itconcept.it",
-  user="hrhackathon",
-  passwd="bwAn5jr5xQbvSYkmnrMVxBa5n2xwCm",
-  database="hrhackathon"
-)
+# #db connection information
+# mydb = mysql.connector.connect(
+#   host=" ",
+#   user=" ",
+#   passwd=" ",
+#   database=" "
+# )
 
 #inserts the data tuple into the db
 def insertData(location,profession,url,lat,lon):
@@ -47,6 +50,7 @@ cities = pd.read_csv('worldcities.csv', sep=',' , na_values=['nan'], na_filter=T
 #filter to only european cities
 eu_cities = cities[cities['country'].isin(europe_countries)]
 eu_cities = eu_cities.fillna(0)
+N_cities = len(eu_cities)
 print('cities filtered')
 
 #list of job titles and proportion to total population
@@ -56,8 +60,9 @@ proportion_jobtitle = [0.000005,0.000002,0.000004,0.000004]
 N_jobs = len(jobtitle)
 #create the number of talents for each city
 talentCount = pd.DataFrame(columns = ['Location','Latitude','Longitude'] + jobtitle)
-for i in range(len(eu_cities)):
-    print(eu_cities['city'].iloc[i])
+
+for i in range(N_cities):
+    #print(eu_cities['city'].iloc[i])
     city_info = {'Location':eu_cities['city'].iloc[i], 'Latitude':eu_cities['lat'].iloc[i],'Longitude':eu_cities['lng'].iloc[i]}
 
     #get the number of talents for each job title
@@ -69,23 +74,31 @@ for i in range(len(eu_cities)):
     city_info.update(talent_info)
     talentCount = talentCount.append(city_info,ignore_index=True)
 
-talentCount.to_csv('output.csv',index=False)
+print('length talent %s' % len(talentCount))
+
+talentCount.to_csv('output.csv', index=False)
+
+F = open('db_copy.csv','w')
+F.write('Location,Job Title,url,Latitude,Longitude\n')
 
 if INSERT == True:
     print(' **** ')
     print('insert to DB')
-    for i in range(len(talentCount)):
+    db_copy_csv = pd.DataFrame(columns=['Location', 'Job Title','url','Latitude','Longitude'])
+    iter=0
+    for i in range(N_cities):
         location = talentCount['Location'].iloc[i]
         latitude = str(talentCount['Latitude'].iloc[i])
         longitude = str(talentCount['Longitude'].iloc[i])
-        print(location)
+        #print(location)
         for j in range(N_jobs):
             iteration = talentCount[jobtitle[j]].iloc[i]
 
-            # print(jobtitle[j])
-            # print(location)
-            # print(latitude)
-            # print(longitude)
-            # print('number of insertion = %s' % iteration)
             for k in range(iteration):
-                insertData(location,jobtitle[j],"http://devpost.com",latitude,longitude)
+                F.write('%s,%s,%s,%s,%s\n' %(location,
+                                             jobtitle[j],
+                                             "http://devpost.com",
+                                             latitude,
+                                             longitude))
+                #insertData(location,jobtitle[j],"http://devpost.com",latitude,longitude)
+    F.close()
